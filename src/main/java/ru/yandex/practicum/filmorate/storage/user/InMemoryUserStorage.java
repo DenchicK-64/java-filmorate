1c339e6e3cb8d0baa4c5b62726816a6ccd1be1b2
Denis S <richardthell64@gmail.com>
2023-05-02 00:40:44 +0400

    ТЗ11. Небольшие исправления.

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -4,11 +4,8 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exceptions.UserNotFoundException;
-import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import javax.validation.Valid;
-import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
 
